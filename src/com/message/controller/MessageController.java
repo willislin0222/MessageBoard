3e91d9c639ff7c4532ca6ddeac8806ed0b16399a
WILLISPC\willis <love0725@gmail.com>
2018-08-22 21:36:28 +0800

    將搜尋資料程式另外寫成一個SearchMessage.java供其他程式重覆呼叫使用

--- src/com/message/controller/MessageController.java
+++ src/com/message/controller/MessageController.java
@@ -34,6 +34,7 @@ import com.message.model.MessageService;
 import com.message.model.MessageVO;
 import com.replymessage.model.ReplyMessageService;
 import com.replymessage.model.ReplyMessageVO;
+import com.searchMessage.SearchMessage;
 
 @Controller
 @RequestMapping("/message")
@@ -181,78 +182,10 @@ public class MessageController {
 			@RequestParam("searchselect") String searchselect,
 			@RequestParam("serachtext") String searchtext) throws IOException{
 		/***************************2.開始查詢資料***************************************/
-		MemberService memberSvc = new MemberService();
-		MessageService messageSvc = new MessageService();
-		ReplyMessageService replymessageSvc = new ReplyMessageService();
-		/***************************3.新增完成,準備轉交(Send the Success view)***********/
-		
-		List<MemberVO> memlist =new ArrayList<MemberVO>();
-		Set<MessageVO> mesSet =new HashSet<MessageVO>();
 		List<MessageVO> meslist =new ArrayList<MessageVO>();
-		List<ReplyMessageVO> reslist =new ArrayList<ReplyMessageVO>();
-		
-		if (searchselect.equals("1")){
-			memlist = memberSvc.getMemberBySearchText(searchtext);
-			for(MemberVO member : memlist){
-				mesSet.addAll(member.getMessages());
-			}
-			meslist.addAll(mesSet);
-			//將meslist做排序
-			Collections.sort(meslist,  new Comparator<MessageVO>(){
-				public int compare(MessageVO o1, MessageVO o2) {
-					// TODO Auto-generated method stub
-					return o2.getMes_date().compareTo(o1.getMes_date());
-				}
-				
-			});
-		}else if(searchselect.equals("2")){
-			mesSet.addAll(messageSvc.getMessageBySearchText(searchtext));
-			meslist.addAll(mesSet);
-			//將meslist做排序
-			Collections.sort(meslist,  new Comparator<MessageVO>(){
-				public int compare(MessageVO o1, MessageVO o2) {
-					// TODO Auto-generated method stub
-					return o2.getMes_date().compareTo(o1.getMes_date());
-				}
-				
-			});
-		}else if(searchselect.equals("3")){
-			memlist = memberSvc.getMemberBySearchText(searchtext);
-			for(MemberVO member : memlist){
-				reslist.addAll(member.getReplymessages());
-			}
-			for(ReplyMessageVO replymessage : reslist){
-				mesSet.add(replymessage.getMessageVO());
-			}
-			
-			meslist.addAll(mesSet);
-			//將meslist做排序
-			Collections.sort(meslist,  new Comparator<MessageVO>(){
-				public int compare(MessageVO o1, MessageVO o2) {
-					// TODO Auto-generated method stub
-					return o2.getMes_date().compareTo(o1.getMes_date());
-				}
-				
-			});
-		}else if (searchselect.equals("4")){
-			reslist = replymessageSvc.getReplyMessageBySearchText(searchtext);
-			for(ReplyMessageVO replymessage : reslist){
-				meslist.add(replymessage.getMessageVO());
-			}
-			
-			meslist.addAll(mesSet);
-			//將meslist做排序
-			Collections.sort(meslist,  new Comparator<MessageVO>(){
-				public int compare(MessageVO o1, MessageVO o2) {
-					// TODO Auto-generated method stub
-					return o2.getMes_date().compareTo(o1.getMes_date());
-				}
-				
-			});
-			
-		}else{
-			meslist.addAll(messageSvc.getAll());
-		}
+		SearchMessage searchMessage = new SearchMessage();
+		meslist = searchMessage.searchMessageBySeacrhtext(searchtext, searchselect);
+		/***************************3.新增完成,準備轉交(Send the Success view)***********/
 		session.setAttribute("messagelist", meslist);
 		return new ModelAndView("message/listSearchMessage","messagelist",meslist);
 			
---
+++ src/com/searchMessage/SearchMessage.java
@@ -0,0 +1,97 @@
+package com.searchMessage;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.member.model.MemberService;
+import com.member.model.MemberVO;
+import com.message.model.MessageService;
+import com.message.model.MessageVO;
+import com.replymessage.model.ReplyMessageService;
+import com.replymessage.model.ReplyMessageVO;
+
+public class SearchMessage {
+	
+	public List<MessageVO> searchMessageBySeacrhtext(String searchtext,String searchselect){
+		
+		MemberService memberSvc = new MemberService();
+		MessageService messageSvc = new MessageService();
+		ReplyMessageService replymessageSvc = new ReplyMessageService();
+		
+		List<MemberVO> memlist =new ArrayList<MemberVO>();
+		Set<MessageVO> mesSet =new HashSet<MessageVO>();
+		List<MessageVO> meslist =new ArrayList<MessageVO>();
+		List<ReplyMessageVO> reslist =new ArrayList<ReplyMessageVO>();
+		
+		if (searchselect.equals("1")){
+			memlist = memberSvc.getMemberBySearchText(searchtext);
+			for(MemberVO member : memlist){
+				mesSet.addAll(member.getMessages());
+			}
+			meslist.addAll(mesSet);
+			//將meslist做排序
+			Collections.sort(meslist,  new Comparator<MessageVO>(){
+				public int compare(MessageVO o1, MessageVO o2) {
+					// TODO Auto-generated method stub
+					return o2.getMes_date().compareTo(o1.getMes_date());
+				}
+				
+			});
+		}else if(searchselect.equals("2")){
+			mesSet.addAll(messageSvc.getMessageBySearchText(searchtext));
+			meslist.addAll(mesSet);
+			//將meslist做排序
+			Collections.sort(meslist,  new Comparator<MessageVO>(){
+				public int compare(MessageVO o1, MessageVO o2) {
+					// TODO Auto-generated method stub
+					return o2.getMes_date().compareTo(o1.getMes_date());
+				}
+				
+			});
+		}else if(searchselect.equals("3")){
+			memlist = memberSvc.getMemberBySearchText(searchtext);
+			for(MemberVO member : memlist){
+				reslist.addAll(member.getReplymessages());
+			}
+			for(ReplyMessageVO replymessage : reslist){
+				mesSet.add(replymessage.getMessageVO());
+			}
+			
+			meslist.addAll(mesSet);
+			//將meslist做排序
+			Collections.sort(meslist,  new Comparator<MessageVO>(){
+				public int compare(MessageVO o1, MessageVO o2) {
+					// TODO Auto-generated method stub
+					return o2.getMes_date().compareTo(o1.getMes_date());
+				}
+				
+			});
+		}else if (searchselect.equals("4")){
+			reslist = replymessageSvc.getReplyMessageBySearchText(searchtext);
+			for(ReplyMessageVO replymessage : reslist){
+				meslist.add(replymessage.getMessageVO());
+			}
+			
+			meslist.addAll(mesSet);
+			//將meslist做排序
+			Collections.sort(meslist,  new Comparator<MessageVO>(){
+				public int compare(MessageVO o1, MessageVO o2) {
+					// TODO Auto-generated method stub
+					return o2.getMes_date().compareTo(o1.getMes_date());
+				}
+				
+			});
+			
+		}else{
+			meslist.addAll(messageSvc.getAll());
+		}
+		
+		return meslist;
+		
+	}
+
+}
